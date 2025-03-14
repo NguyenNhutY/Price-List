24d7d05ccba43f529632ae012666da5b61b838d1
NguyenNhutY <130396826+NguyenNhutY@users.noreply.github.com>
2025-03-14 09:18:19 +0700

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Price-List/", // Đổi your-repo-name thành tên repo của bạn
+
   plugins: [react()],
 })
